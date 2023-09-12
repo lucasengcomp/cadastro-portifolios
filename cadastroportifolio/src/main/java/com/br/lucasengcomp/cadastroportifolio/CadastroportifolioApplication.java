2c3adf74412badc6936d8af50ff03eea452728ae
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-12 16:07:15 -0300

    :tada: Projeto criado

---
+++ cadastroportifolio/src/main/java/com/br/lucasengcomp/cadastroportifolio/CadastroportifolioApplication.java
@@ -0,0 +1,13 @@
+package com.br.lucasengcomp.cadastroportifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroportifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroportifolioApplication.class, args);
+	}
+
+}
---
+++ cadastroportifolio/src/test/java/com/br/lucasengcomp/cadastroportifolio/CadastroportifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lucasengcomp.cadastroportifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroportifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
